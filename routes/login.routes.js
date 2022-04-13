8bbf91a00242a392d258e0c8dcde2a7bf666ff66
MiguelAreias2 <79462661+MiguelAreias2@users.noreply.github.com>
2022-04-13 17:54:50 +0100

    2

--- routes/login.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { loginView } = require('../controllers/login.controller');
-const router = express.Router();
-module.exports = function(app) {
-    router.get("/login", loginView);
-  
-    app.use('/', router);
-  };
