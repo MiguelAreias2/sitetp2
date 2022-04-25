5159ddda23b554f359d266deadf941af867e00a8
MiguelAreias2 <79462661+MiguelAreias2@users.noreply.github.com>
2022-04-25 22:33:34 +0100

    mudar o logout para um href

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const api = express()
-const port = 3000
+const port = 80
 const cors = require('cors')
 const controller = require("./controllers/user.controller");
 
