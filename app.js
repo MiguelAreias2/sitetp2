b8a4856fa17229276a76c70dcc4ef22577062095
MiguelAreias2 <79462661+MiguelAreias2@users.noreply.github.com>
2022-04-26 23:14:02 +0100

    certificado a dar

--- app.js
+++ app.js
@@ -6,18 +6,12 @@ const cors = require('cors')
 const https = require("https");
 const controller = require("./controllers/user.controller");
 const fs = require("fs");
-var key = fs.readFileSync('selfsigned.key');
-var cert = fs.readFileSync('selfsigned.crt');
-var options = {
-  key: key,
-  cert: cert
-};
-//app.set('view engine', 'ejs')
 const bodyParser = require("body-parser");
 const db = require("./models");
+
+
 db.sequelize.sync();
 //mudar para a pasta local
-app.use(express.static("C:/Users/migue/Desktop/6º SEMESTRE/SEGURANÇA DE REDES E SISTEMAS/sitetp2/views/"))
 // parse requests of content-type - application/json
 api.use(bodyParser.json());
 api.use(cors())
@@ -37,15 +31,13 @@ api.listen(3001, ()=>{
   console.log('API')
 })
 
-// const options = {
-//   key: fs.readFileSync("server.key"),
-//   cert: fs.readFileSync("server.cert")
-// };
 
-var server = https.createServer(options, app);
-server.listen(port, () => {
-  console.log("server starting on port : " + port)
-});
-/*app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})*/
+const path = require('path')
+app.use(express.static("C:/Users/migue/Desktop/6º SEMESTRE/SEGURANÇA DE REDES E SISTEMAS/sitetp2/views/"))
+const sslServer = https.createServer({
+  key:fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+  cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem'))
+},app)
+
+sslServer.listen(port, ()=> console.log('Secure Server'))
+
